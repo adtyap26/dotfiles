57ab9dde08d9400f91e5a795474bbbcd03dee423
Aditya Permana <permanaaditya2606@gmail.com>
2022-12-09 15:06:26 +0700

    Add script

--- Scripts/editformat.sh
+++ Scripts/editformat.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Ask the user to select a variable using dmenu or rofi
-selected=$(echo -e "1080\n720\n480" | dmenu -p "Please Select Quality: ")
+selected=$(echo -e "1080\n720\n480\n360\n240" | dmenu -p "Please Select Quality: ")
 
 # Set the selected variable
 if [ "$selected" == "1080" ]; then
-  variable="1080"
+  variable="1080" && notify-send '1080p Youtube Quality' --icon=dialog-information
 elif [ "$selected" == "720" ]; then
-  variable="720"
+  variable="720" && notify-send '720p Youtube Quality' --icon=dialog-information
 elif [ "$selected" == "480" ]; then
-  variable="480"
+  variable="480" && notify-send '480p Youtube Quality' --icon=dialog-information
+elif [ "$selected" == "360" ]; then
+  variable="360" && notify-send '360p Youtube Quality' --icon=dialog-information
+elif [ "$selected" == "240" ]; then
+  variable="240" && notify-send '240p Youtube Quality' --icon=dialog-information
 fi
 
 # Replace the value of the local variable "changedQuality" with the selected variable
-# in the file "file.txt" and save the changes to the same file
 sed -i "s/\(changedQuality = \)[0-9]\+/\1$variable/g" ~/.config/mpv/scripts/ytdlautoformat.lua
 
