6315302bf642a72833988d043ce424e855627e37
Aditya Permana <permanaaditya2606@gmail.com>
2023-07-09 10:49:01 +0700

    add scripts

--- Scripts/warpcontrol.sh
+++ Scripts/warpcontrol.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-
-# set -euo pipefail
-# # Display options for controlling warp-cli in rofi
-# # option=$(echo -e " connect\n disconnect" | rofi -dmenu -p " Warp control" -theme-str 'window {width: 20%;height: 20%;}')
-# option=$(echo -e " connect\n disconnect" | dmenu -i -p " Warp control")
-#
-# # Connect or disconnect from warp-cli based on the selected option
-# if [[ $option = "connect" ]]; then
-#   warp-cli connect 
-#   notify-send -i /usr/share/icons/Sardi/scalable/status/network-vpn.svg "Connected to warp!"
-# elif [[ $option = "disconnect" ]]; then
-#   warp-cli disconnect 
-#   notify-send -i /usr/share/icons/Sardi/scalable/status/network-offline-symbolic.svg "Disconnecting warp!"
-# else
-#   exit 0
-# fi
-
-
-
 # Display options for controlling warp-cli in rofi
-option=$(echo -e "connect\ndisconnect" | rofi -dmenu -p " Warp control" -theme-str 'window {width: 20%;height: 20%;}')
-# option=$(echo -e " connect\n disconnect" | dmenu -i -p " Warp control")
+# option=$(echo -e "connect\ndisconnect" | rofi -dmenu -p " Warp control" -theme-str 'window {width: 20%;height: 20%;}')
+option=$(echo -e " Connect\n Disconnect" | dmenu -p " Warp control")
+
+# option=`zenity --list --text=" Warp Cloudflare 1.1.1.1" --column="Select your choice:" " Connect" " Disconnect"` # Using zenity
 
 # Connect or disconnect from warp-cli based on the selected option
-if [[ $option == "connect" ]]; then
-  warp-cli connect
-  notify-send -i /usr/share/icons/Sardi/scalable/status/network-vpn.svg "Connected to warp!"
-elif [[ $option == "disconnect" ]]; then
-  warp-cli disconnect
-  notify-send -i /usr/share/icons/Sardi/scalable/status/network-offline-symbolic.svg "Disconnected to warp!"
+if [[ "$option" == " Connect" ]]; then
+	warp-cli connect
+	# notify-send -i /usr/share/icons/Sardi/scalable/status/network-vpn.svg "Connected to warp!"
+	notify-send -i ~/Apps/Tokyo-Night-GTK-Theme/icons/Tokyonight-Dark/status/symbolic/network-vpn-symbolic.svg "Connected to warp!"
+elif [[ "$option" == " Disconnect" ]]; then
+	warp-cli disconnect
+	# notify-send -i /usr/share/icons/Sardi/scalable/status/network-offline-symbolic.svg "Disconnected to warp!"
+	notify-send -i ~/Apps/Tokyo-Night-GTK-Theme/icons/Tokyonight-Dark/status/symbolic/network-offline-symbolic.svg "Disconnected to warp!"
 else
-  exit 0
+	exit 0
 fi
-
